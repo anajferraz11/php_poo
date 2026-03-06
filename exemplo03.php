6a6c82d93f177fc3b89dcb67076cd079e3d66f9e
anajferraz11 <ana.j.melo11@aluno.senai.br>
2026-03-06 11:12:21 -0300

    finalizado

--- exemplo03.php
+++ exemplo03.php
@@ -1,6 +1,6 @@
 <?php
 function calcularDescontos($salario, $previdencia){
-    return number_format($salrio*0.275 + $previdencia, 2,',', '.');
+    return number_format($salario*0.275 + $previdencia, 2, ',', '.');
 }
 
 $funcionarios = array(3);
@@ -9,9 +9,9 @@ $funcionarios[1] = array('Maria Rute', 2000,200);
 $funcionarios[2] = array('José Salgado', 3000,300);
 
 for($i = 0; $i < 3; $i++){
-    $nome = $funcionarios[$1][0];
+    $nome = $funcionarios[$i][0];
     $descontos = calcularDescontos($funcionarios[$i][1], $funcionarios[$i][2]);
-}
- echo "O Valor do desconto de $nome é de R$ $descontos. <br>";
+    echo "O Valor do desconto de $nome é de R$ $descontos. <br>";
 
+}
 ?>
