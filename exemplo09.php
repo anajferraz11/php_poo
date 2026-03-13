b56badefc42ca4071a5c880f1d656d478c91a3c5
anajferraz11 <ana.j.melo11@aluno.senai.br>
2026-03-13 14:44:29 -0300

    finalizado

--- exemplo09.php
+++ exemplo09.php
@@ -16,4 +16,10 @@ class Filme
         $this->saldo = $this->saldo + $valor;
     }
 }
+
+$filme = new Filme("Toy Story", 10);
+$filme->incrementarSaldo(5);
+
+echo "Nome do Filme: $filme->nome<br>";
+echo "Saldo disponível: $filme->saldo";
 ?>
