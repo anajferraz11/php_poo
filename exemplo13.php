b5487ef21f0b16df2a7e375bbeb472fd0ba9da93
anajferraz11 <ana.j.melo11@aluno.senai.br>
2026-04-15 08:43:37 -0300

    finali

--- exemplo13.php
+++ exemplo13.php
@@ -34,7 +34,14 @@ class Conta
 
 class Poupanca extends Conta
 {
-    public $juros = 0.05;
+    public $juros;
+
+    function __construct($numero, $saldo, $juros)
+    {
+        $this->numero = $numero;
+        $this->saldo = $saldo;
+        $this->juros = $juros;
+    }
 
     function atualizarJuros()
     {
@@ -47,7 +54,7 @@ $conta->creditar(50);
 $conta->debitar(100);
 echo "Saldo da conta $conta->numero: $conta->saldo <br>";
 
-$poupanca = new Poupanca(2, 150);
+$poupanca = new Poupanca(2, 150, 0.10);
 $poupanca->creditar(50);
 $poupanca->debitar(100);
 $poupanca->atualizarJuros();
--- exemplo14.php
+++ exemplo14.php
@@ -34,7 +34,13 @@ class Conta
 
 class Poupanca extends Conta
 {
-    public $juros = 0.05;
+    public $juros;
+
+    function __construct($numero, $saldo, $juros)
+    {
+        parent::__construct($numero, $saldo);
+        $this->juros = $juros;
+    }
 
     function atualizarJuros()
     {
@@ -47,7 +53,7 @@ $conta->creditar(50);
 $conta->debitar(100);
 echo "Saldo da conta $conta->numero: $conta->saldo <br>";
 
-$poupanca = new Poupanca(2, 150);
+$poupanca = new Poupanca(2, 150, 0.10);
 $poupanca->creditar(50);
 $poupanca->debitar(100);
 $poupanca->atualizarJuros();
